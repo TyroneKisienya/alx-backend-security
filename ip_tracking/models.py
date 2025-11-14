73ad846deeccc84f6722f05d4c80bb45f64756f3
Skai <tyronekisienya01@gmail.com>
2025-11-14 13:32:03 +0300

    initial commit plus model created

---
+++ ip_tracking/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class requesting(models.Model):
+    ip_address = models.GenericIPAddressField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    path = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.path} at {self.timestamp}"
