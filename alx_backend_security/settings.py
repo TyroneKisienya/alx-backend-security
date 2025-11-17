4a3875825a417f5f773ff7006fbe99fb8db84c4a
Skai <tyronekisienya01@gmail.com>
2025-11-17 14:51:40 +0300

    new commit

--- alx_backend_security/settings.py
+++ alx_backend_security/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from celery.schedules import crontab
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,3 +151,5 @@ CACHES = {
 
 RATE_LIMIT_USE_CACHE = "default"
 
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
